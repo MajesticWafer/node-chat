bb72669d0ef1525b371520709b00b97ed1e53f00
MajesticWafer <123651667+MajesticWafer@users.noreply.github.com>
2024-12-02 08:50:40 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Serve static files
+app.use(express.static('public'));
+
+// Handle socket connection
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    // Broadcast received message to all clients
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+// Start the server
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
